d3b04254580d10afe49d990378f8ad5c8b921172
kanav789 <kanavrana91@gmail.com>
2024-12-28 12:39:28 +0530

    Adding the color and fonts name in tailwind config file for using in tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purpleText: "#272362",
+        whitecolor: "#FFFFFF",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], // Add Poppins as a custom font
+      },
+    },
   },
   plugins: [],
 };
