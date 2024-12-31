a881d55936614b8c122c4813476d1049e1bf065e
Pratik <shampachghare@gmail.com>
2024-12-31 13:41:00 +0530

    hone commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Home from "./Pages/Home";
+import Navbar from "./Componets/Home/Navbar";
+import Footer from "./Componets/Home/Footer";
 function App() {
   return (
     <div>
+      <Navbar/>
       <Outlet />
+      <Footer/>
     </div>
   );
 }
