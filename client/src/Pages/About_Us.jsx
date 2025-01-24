4736e434fbb7217a4e6088cbbccfe9fda51c8a0b
kanav789 <kanavrana91@gmail.com>
2025-01-24 23:07:03 +0530

    adding the mesage that this pages are under construction

--- client/src/Pages/About_Us.jsx
+++ client/src/Pages/About_Us.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function About_Us() {
   return (
-    <div>
-      About_us
+    <div className="mt-[15vh] font-medium text-xl flex justify-center">
+      About_us, Pages is under construction
     </div>
-  )
+  );
 }
